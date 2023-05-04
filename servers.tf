43e00f3e472b1867e854a273dda2cce8a5f7151e
Jyothsna <jyothsnamandlipalli@gmail.com>
2023-05-04 14:17:27 +0300

    roboshop basic

---
+++ servers.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "frontend" {
+  ami           = var.ami #this is centos-8 ami id
+  instance_type = var.instance_type
+
+ // tags = {
+   // Name = merge(var.tags,{
+     // Name = "frontend"
+    //})
+  //}
+  tags = {
+    Name = "frontend"
+  }
+}
+
