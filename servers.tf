2a2b1ff5b2452b82cdb92a57f42777624edbcb9c
Jyothsna <jyothsnamandlipalli@gmail.com>
2023-05-04 15:10:10 +0300

    roboshop basic

--- servers.tf
+++ servers.tf
@@ -1,12 +1,23 @@
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2" #this is centos-8 ami id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
+    Name = frontend
   }
 }
 
-output "frontend" {
-  value = aws_instance.frontend.public_ip
-}
+
+
+
